6e776b082623e71afceab323f5e0b9aa7f202b07
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-01 20:50:18 +0500

    clean up: removing duplicates, clean up

--- src/main/java/com/will/simulation/Main.java
+++ src/main/java/com/will/simulation/Main.java
@@ -2,18 +2,15 @@ package com.will.simulation;
 
 /* TODO:
     1. Learn Multithreading, simulating running of main program and waiting another thread for an input
-    2. Adding options (start, pause, resume, exit)
-    3. Deploy the program and send it to reviewing
+    2. Populating map with the exact proportions of entities
+    3. Move the logic for eating and moving in (herbivore, predator) classes into creature, remove duplicating
+    4. Adding options (start, pause, resume, exit)
+    5. Deploy the program and send it to reviewing
  */
 
 public class Main {
     public static void main(String[] args) {
-
-        World world = new World(12, 12);
-        Action action = new Action(world);
-        action.initActions();
-        WorldRenderer worldRenderer = new WorldRenderer();
-        Simulation simulation = new Simulation(world, worldRenderer, action);
+        Simulation simulation = new Simulation(new World(12, 12));
         simulation.startSimulation();
     }
 }
